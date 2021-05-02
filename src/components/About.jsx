a752dade8ddb61588427ec18b51b38fef1b9bd5b
MayarElMahdy <elmahdymayar@gmail.com>
2021-05-02 02:20:07 +0200

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import Web3 from 'web3';
 import { Link, withRouter } from "react-router-dom";
 
@@ -37,8 +38,9 @@ class About extends React.Component{
         this.setState({account : accounts[0]})
       }
 
-      handle_output()
+      handle_output = (event) => 
       {
+        event.preventDefault();
         var title = document.getElementById("Contract-name").value 
         var can1 = document.getElementById("Cand-1").value
         var can2 = document.getElementById("Cand-2").value
@@ -48,7 +50,6 @@ class About extends React.Component{
         var date2 = document.getElementById("date_Reg_End").value
         var time3 = document.getElementById("time_Vote_End").value
         var date3 = document.getElementById("date_Vote_End").value
-        console.log("I AM HERE")
         if(title && can1 && can2 && time1 && time2 && time3 && date1 && date2 && date3)
         {
           alert("SUCESSSSSS") 
@@ -58,9 +59,11 @@ class About extends React.Component{
         {
           
           alert("Empty")
+          
         }
 
       }
+      
     
       constructor(props)
       {
@@ -74,11 +77,13 @@ class About extends React.Component{
   render(){
     
     return (
-    <div>
+    
+    <div >
       
-        <div className="forms">
-          <h3 style={{textAlign: 'center'}}>Please fill out this form to create ballot {this.state.account}</h3>  
-          <form >
+        <div >
+           
+          <form onSubmit = {this.handle_output.bind(this)} className= "form">
+            <br></br>
             <label htmlFor="Contract-name">Ballot Name</label>
             <input type="text" id="Contract-name" name="Ballot Name" placeholder="Enter the name of ballot" />
             <br />
@@ -90,33 +95,38 @@ class About extends React.Component{
             <br />
             <label htmlFor="TimeStamps">Enter the timestamp of each phase : <br /> </label>
             <br /><br />
-            <label htmlFor="Reg-Time">Registration phase start</label> 
+            <label htmlFor="Reg-Time">Registration phase start &emsp;</label> 
             <input id="date_Reg_Start" type="date" />
             <input id="time_Reg_Start" type="time" />
-            <br /><br />
-            <label htmlFor="Reg-Time">Registration phase end</label>
+            
+            <label htmlFor="Reg-Time">&emsp;Registration phase end&emsp;</label>
             <input id="date_Reg_End" type="date" />
             <input id="time_Reg_End" type="time" />
+            
+            <label htmlFor="Reg-Time" style={{color: '#db1818'}}>&emsp;Voting phase will start automatically after registration ends</label>
             <br /><br />
-            <label htmlFor="Reg-Time" style={{color: '#db1818'}}>Voting phase will start automatically after registration ends</label>
-            <br /><br />
-            <label htmlFor="Reg-Time">Voting phase end</label>
+            <label htmlFor="Reg-Time">Voting phase end &emsp;</label>
             <input id="date_Vote_End" type="date" />
             <input id="time_Vote_End" type="time" />
-            <br /> <br />
-            <label htmlFor="Tally" style={{color: '#db1818'}}>Tally phase will start automatically after voting phase is over </label>
+            
+            <label htmlFor="Tally" style={{color: '#db1818'}}>&emsp;Tally phase will start automatically after voting phase is over </label>
             <br /><br />
             
             <label htmlfor="myfile">Select a text file containing the eligible voter's addresses: </label>
             <br></br>
             <input type="file" id="myfile" name="myfile"></input>
-            
-            <Link to="/" type="submit" className="Button" onClick={this.handle_output.bind()}>Create Ballot</Link>
+            <br></br>
+            <br></br>
+            <input type= "submit"/>
+            <Link to="/" type="submit" className="Button" >Create Ballot</Link>
             
             
           </form>
+
         </div>
-        <div className="question-main" /> 
+        
+        
+       
       </div>
       
     
